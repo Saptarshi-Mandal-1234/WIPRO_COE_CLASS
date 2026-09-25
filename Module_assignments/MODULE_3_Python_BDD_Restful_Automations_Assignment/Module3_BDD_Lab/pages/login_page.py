682ac9028dc63e86bd1913d3abfe46ab8018dcd5
Saptarshi Mandal <78576282+Saptarshi-Mandal-1234@users.noreply.github.com>
2026-09-25 14:45:38 +0530

    Add files via upload

---
+++ Module_assignments/MODULE_3_Python_BDD_Restful_Automations_Assignment/Module3_BDD_Lab/pages/login_page.py
@@ -0,0 +1,24 @@
+from selenium.webdriver.common.by import By
+
+
+class LoginPage:
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def open(self):
+        self.driver.get("https://www.saucedemo.com/")
+
+    def enter_username(self, username):
+        self.driver.find_element(By.ID, "user-name").send_keys(username)
+
+    def enter_password(self, password):
+        self.driver.find_element(By.ID, "password").send_keys(password)
+
+    def click_login(self):
+        self.driver.find_element(By.ID, "login-button").click()
+
+    def get_error_message(self):
+        return self.driver.find_element(
+            By.CSS_SELECTOR, "[data-test='error']"
+        ).text
